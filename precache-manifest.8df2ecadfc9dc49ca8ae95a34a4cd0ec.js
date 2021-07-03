cc49a8fe533cff92958f53edf7ac767164998e30
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2021-07-03 12:41:17 +0700

    first commit

---
+++ precache-manifest.8df2ecadfc9dc49ca8ae95a34a4cd0ec.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be744cadb50a3eb4af5f",
+    "url": "/css/app.cb8f5324.css"
+  },
+  {
+    "revision": "79ddbc1c0daa93c42333",
+    "url": "/css/chunk-vendors.468c17d3.css"
+  },
+  {
+    "revision": "fadc7e7eab9f87b94c7cfc10dd385f59",
+    "url": "/img/logo.fadc7e7e.png"
+  },
+  {
+    "revision": "7b96ed8c0a4b6815e0089abaeb09a7f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be744cadb50a3eb4af5f",
+    "url": "/js/app.01ec8229.js"
+  },
+  {
+    "revision": "79ddbc1c0daa93c42333",
+    "url": "/js/chunk-vendors.cc0295a7.js"
+  },
+  {
+    "revision": "ed53ef2d6f1a8197e925a6caf8069193",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
