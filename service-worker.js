216d7e12c5f61410315644cfd235c83a799d6eea
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2021-07-07 22:43:10 +0700

    update link

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e65993316ae88e6c9659cb08e5750f73.js"
+  "/precache-manifest.ebee8d9b6119517ebc63dfefb0af9a27.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "tmc-exam"});
